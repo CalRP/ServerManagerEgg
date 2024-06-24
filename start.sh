bfd17dec2a129d11389904eb0a0d2c309b808b02
Avery Johnson <avery627@gmail.com>
2024-06-24 02:06:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,14 @@ BLUE='\033[0;34m'
 
 Text="${GREEN}[STARTUP]${NC}"
 
+# Check if "node" command is available
+if ! command -v node &> /dev/null; then
+    echo -e "${Text} ${RED}Error: Node.js is not installed or not in the PATH.${NC}"
+    exit 1
+fi
+
+echo -e "${Text} ${BLUE}Node.js is available.${NC}"
+
 echo -e "${Text} ${BLUE}Starting checks for all updates...${NC}"
 RELEASE_PAGE=$(curl -sSL https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/)
 CHANGELOGS_PAGE=$(curl -sSL https://changelogs-live.fivem.net/api/changelog/versions/linux/server)
@@ -29,4 +37,4 @@ fi
 
 echo -e "${Text} ${BLUE}Starting FiveM Server...${NC}"
 
-$(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey ${FIVEM_LICENSE} +set steam_webApiKey ${STEAM_WEBAPIKEY} +set sv_maxplayers ${MAX_PLAYERS} +set serverProfile default +set txAdminPort ${TXADMIN_PORT} $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
+node $(pwd)/middleware.js $(pwd)/alpine/opt/cfx-server/ld-musl-x86_64.so.1 --library-path "$(pwd)/alpine/usr/lib/v8/:$(pwd)/alpine/lib/:$(pwd)/alpine/usr/lib/" -- $(pwd)/alpine/opt/cfx-server/FXServer +set citizen_dir $(pwd)/alpine/opt/cfx-server/citizen/ +set sv_licenseKey ${FIVEM_LICENSE} +set steam_webApiKey ${STEAM_WEBAPIKEY} +set sv_maxplayers ${MAX_PLAYERS} +set serverProfile default +set txAdminPort ${TXADMIN_PORT} $( [ "$TXADMIN_ENABLE" == "1" ] || printf %s '+exec server.cfg' )
